75d8897972254ceb6b0706f33c99d9fa90d79140
Tobias Gurdan <tobias.gurdan@intel.com>
2019-05-17 13:26:59 +0200

    Add convenience Color support to encodings

--- include/vivid/encoding.h
+++ include/vivid/encoding.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <string>
 #include "vivid/conversion.h"
+#include "vivid/color.h"
+#include <string>
 
 
 namespace tq::ansi
@@ -12,6 +13,14 @@ namespace tq::ansi
 
     inline std::string bg( const uint8_t index ) {
         return "\x1b[48;5;" + std::to_string( index ) + "m";
+    }
+
+    inline std::string fg( const Color& col ) {
+        return "\x1b[38;5;" + std::to_string( col.index() ) + "m";
+    }
+
+    inline std::string bg( const Color& col ) {
+        return "\x1b[48;5;" + std::to_string( col.index() ) + "m";
     }
 
     static const std::string reset = "\x1b[0m";
@@ -46,6 +55,14 @@ namespace tq::html
             std::to_string( rgb8.y ) + ", " +
             std::to_string( rgb8.z ) + ")'>"
         );
+    }
+
+    inline std::string fg( const Color& col ) {
+        return fg( col.rgb8() );
+    }
+
+    inline std::string bg( const Color& col ) {
+        return bg( col.rgb8() );
     }
 
     static const std::string close = "</span>";
