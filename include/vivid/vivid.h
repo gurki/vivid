f4007fd41b18e70f8ca4bbdf6eeb1ba728fa8f56
Tobias Gurdan <tobias@gurdan.de>
2019-05-24 01:09:17 +0200

    Simplify and sort attributions

--- include/vivid/vivid.h
+++ include/vivid/vivid.h
@@ -3,23 +3,25 @@
 //
 //  kudos to all you awesome color enthusiasts out there <3
 //
-//  [1] http://docs.opencv.org/3.1.0/de/d25/imgproc_color_conversions.html#gsc.tab=0
-//  [2] http://www.easyrgb.com/index.php?X=MATH&H=08#text8
-//  [3] https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-//  [4] https://www.cs.rit.edu/~ncs/color/t_convert.html
-//  [5] https://www.vis4.net/blog/2011/12/avoid-equidistant-hsv-colors/
-//  [6] https://github.com/mbostock/d3/blob/master/src/color/rgb.js
-//  [7] https://github.com/gawin/bash-colors-256
-//  [8] https://jonasjacek.github.io/colors/
-//  [9] https://github.com/BIDS/colormap
-//  [10] http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
-//  [11] https://www.rapidtables.com/convert/color/hsl-to-rgb.html
-//  [12] https://docs.rs/crate/ansi_colours/1.0.1/source/src/ansi256.c
+//    http://docs.opencv.org/3.1.0/de/d25/imgproc_color_conversions.html#gsc.tab=0
+//    http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+//    http://www.easyrgb.com/index.php?X=MATH&H=08#text8
+//    https://convertingcolors.com
+//    https://docs.rs/crate/ansi_colours/1.0.1/source/src/ansi256.c
+//    https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+//    https://github.com/BIDS/colormap
+//    https://github.com/gawin/bash-colors-256
+//    https://github.com/mbostock/d3/blob/master/src/color/rgb.js
+//    https://jonasjacek.github.io/colors/
+//    https://www.cs.rit.edu/~ncs/color/t_convert.html
+//    https://www.rapidtables.com/convert/color/hsl-to-rgb.html
+//    https://www.vis4.net/blog/2011/12/avoid-equidistant-hsv-colors/
 //
 
 #include "vivid/color.h"
 #include "vivid/colormap.h"
 #include "vivid/conversion.h"
+#include "vivid/data.h"
 #include "vivid/encoding.h"
 #include "vivid/functions.h"
 #include "vivid/fuzzy.h"
